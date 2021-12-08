b16178d483325957cdb2c93600a2fdc00aa8864f
Haris Gušić <harisgusic.dev@gmail.com>
2021-12-08 10:52:42 +0100

    Disable vue host check
    Signed-off-by: Haris Gušić <harisgusic.dev@gmail.com>

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,5 +4,6 @@ module.exports = {
     devServer: {
         // Write files to disk in dev mode, so Django can serve the assets
         writeToDisk: true,
+        disableHostCheck: true,
     },
 }
