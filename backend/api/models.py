6c28af7a97f68738e6b080655a3da7f8ffddc73c
Haris Gušić <harisgusic.dev@gmail.com>
2022-03-27 15:27:27 +0200

    Add project model and migrations
    Signed-off-by: Haris Gušić <harisgusic.dev@gmail.com>

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
+
 class Project(models.Model):
-    slug = models.CharField(primary_key=True, max_length=50)
-    title = models.CharField(max_length=100)
-    url = models.CharField(max_length=100)
-    start_date = models.DateField(auto_now_add=True)
-    end_date = models.DateField(auto_now_add=True)
+    id = models.IntegerField(primary_key=True)
+    slug = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    desc = models.TextField()
+    url = models.URLField()
+    repo_url = models.URLField(null=True)
+    start_date = models.DateField(null=True)
     organization = models.CharField(max_length=50, null=True)
-    my_contributions_url = models.CharField(max_length=300, null=True)
+    organization_url = models.CharField(max_length=100, null=True)
+    my_contributions_url = models.URLField(null=True)
+    roles = models.JSONField(max_length=200, null=True)
+    languages = models.JSONField(null=True)
