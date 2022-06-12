5b8ab1af67d5ea3f67cffe414feab9de6533906f
Haris Gušić <harisgusic.dev@gmail.com>
2022-06-13 00:11:04 +0200

    Update 'clean' script and add some comments
    Signed-off-by: Haris Gušić <harisgusic.dev@gmail.com>

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 
-# Because the frontend directory on the host is being shared with the container
-# as a volume, we have to re-populate the node_modules directory at runtime.
-# This is a cheap operation since the modules have already been installed during
-# docker build.
+# Because the project root directory on the host is being shared with the
+# container as a volume, we have to re-populate the node_modules directories at
+# runtime. This is a cheap operation since the modules have already been
+# installed during docker build, so pnpm wll simply create hardlinks to them.
 pnpm install --frozen-lockfile --no-verify-store-integrity --dir frontend
+pnpm install --frozen-lockfile --no-verify-store-integrity --dir worker
 
 pnpm --dir frontend run dev --host &
 
