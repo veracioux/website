671920752ae764955792d1bd28f3206b7951b32e
Haris Gušić <harisgusic.dev@gmail.com>
2022-08-05 01:02:24 +0200

    Set up docker registry

--- .tem/env/@fish/completions.fish
+++ .tem/env/@fish/completions.fish
@@ -1,5 +1,7 @@
-set -l services web db rabbitmq worker_dotfiles
+set -l services web db rabbitmq worker_dotfiles mail docker-registry
 
 complete -c up   -f -a "$services"
 complete -c down -f -a "$services"
 complete -c into -f -a "$services"
+
+complete -c lpass -f -a "(ls (tem find -b veracioux-website)/.tem/files/passwords | string replace .gpg '')"
