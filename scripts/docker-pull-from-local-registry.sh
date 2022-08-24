84def94311ae194ec12667af496bd2130b6da4f8
Haris Gušić <harisgusic.dev@gmail.com>
2022-08-25 00:37:18 +0200

    Update upload script to include staging images

--- scripts/docker-pull-from-local-registry.sh
+++ scripts/docker-pull-from-local-registry.sh
@@ -2,7 +2,13 @@
 
 # Pull all website-related images from the locally running registry
 
-for image in {web,worker_dotfiles,mail}:latest; do
+if [ "$ENVIRONMENT" = "staging" ]; then
+    tag="staging"
+else
+    tag="latest"
+fi
+
+for image in {web,worker_dotfiles,mail}:"$tag"; do
     echo "Pulling image localhost:5000/$image from local registry into daemon..."
     docker pull localhost:5000/"$image"
     echo "Re-tagging image as veracioux.me/$image"
--- scripts/serve.sh
+++ scripts/serve.sh
@@ -5,20 +5,20 @@
 . scripts/load-env.bash.in
 
 # If the nginx config is sound, back it up
-if nginx -t; then
+if nginx -t 2>/dev/null; then
     cp /etc/nginx/nginx.conf{,.bak}
 fi
 
 cat nginx.host.conf.in | envsubst '$WEB_PORT,$WEB_PORT_STAGING,$USER_HOME,$DOCKER_REGISTRY_PORT' > /etc/nginx/nginx.conf
 
-if nginx -t; then
+if nginx -t 2>/dev/null; then
     pkill nginx
     nginx
     echo "Successfully started nginx." >&2
 else
     cp /etc/nginx/nginx.conf{.bak,}
     echo "Error: nginx config invalid." >&2
-    if nginx -t; then
+    if nginx -t 2>/dev/null; then
         echo "Starting nginx using previous config..." >&2
         nginx
     fi
