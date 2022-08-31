933dc8e665cf7ef92be47bebb8b0014404eba791
Haris Gušić <harisgusic.dev@gmail.com>
2022-08-31 23:18:49 +0200

    Extract Bio into own component

--- frontend/src/components/sections/About.vue
+++ frontend/src/components/sections/About.vue
@@ -6,7 +6,7 @@ import Workflow from "@/components/about/Workflow.vue";
 import {ScrollData} from "@/inject";
 import {computed, onMounted, ref} from "vue";
 import CliEffect from "@/components/CliEffect.vue";
-import Pager from "@/components/about/Pager.vue";
+import Bio from "@/components/about/Bio.vue";
 import {mapRange} from "@/utils";
 
 const scrollData = ScrollData.inject();
@@ -116,64 +116,7 @@ onMounted(() => {
                         text="print(self.__doc__)"
                         class="bioCliEffect"
                     >
-                        <Pager class="bio" :progress="checkpoints.bioPager">
-                            <div>
-                                Hi, I'm Haris. I'm a programmer, engineer, and
-                                creator of wonderful things. Since I was a kid
-                                I've had a fascination with science and
-                                engineering. The first time I ran a computer
-                                program, I immediately wanted to create my own.
-                                I got started programming by making Minecraft
-                                mods in Java when I was about 12.
-                                <br /><br />
-                                In high school I discovered C++ and fell in love
-                                with it. I bought an Arduino and played with it
-                                endlessly. I used it in an
-                                <a
-                                    href="https://github.com/veracioux/rotating-led-display"
-                                    target="_blank"
-                                    >interesting little project</a
-                                >. <br /><br />
-                                I also made a
-                                <a
-                                    href="https://github.com/veracioux/wood-fall"
-                                    target="_blank"
-                                    >2D Android game</a
-                                >
-                                in Unity. That was fun, though looking back I'm
-                                a little embarrassed by some stylistic choices.
-                                <br /><br />
-                                Then came college. I got a Bachelor's degree in
-                                Automation and Electronics. During that time I
-                                fell out of programming due to focusing on my
-                                studies. But I rediscovered it again in my third
-                                year and decided to take my career in that
-                                direction.
-                                <br /><br />
-                                Around that time I switched to Linux and learned
-                                a great deal about it. I enhanced my workflow
-                                with powerful and esoteric tools like
-                                <a
-                                    href="https://github.com/veracioux/dotfiles"
-                                    target="_blank"
-                                    >vim, emacs, tiling window managers and I
-                                    made the terminal my cozy new home</a
-                                >. I have been
-                                <a
-                                    href="https://github.com/veracioux"
-                                    target="_blank"
-                                    >actively contributing</a
-                                >
-                                to open source projects, mainly original
-                                projects but also to an established popular
-                                project called
-                                <a
-                                    href="https://github.com/flameshot-org/flameshot"
-                                    target="_blank"
-                                    >Flameshot</a
-                                >.
-                            </div>
-                        </Pager>
+                        <Bio :progress="checkpoints.bioPager" />
                     </CliEffect>
                     <CliEffect
                         :progress="checkpoints.workflowCli"
@@ -328,11 +271,6 @@ onMounted(() => {
 
                 .bioCliEffect :deep(.cli) {
                     padding: 0;
-                }
-
-                .bio {
-                    max-width: 600px;
-                    text-align: justify;
                 }
             }
         }
