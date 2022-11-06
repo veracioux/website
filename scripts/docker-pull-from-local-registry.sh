9bfc31d6e5885e5062b4378cf2563559c91b1827
Haris Gušić <harisgusic.dev@gmail.com>
2022-11-06 20:53:33 +0100

    Move docker registry to docker.* subdomain

--- scripts/docker-pull-from-local-registry.sh
+++ scripts/docker-pull-from-local-registry.sh
@@ -11,7 +11,7 @@ fi
 for image in {web,worker_dotfiles,mail}:"$tag"; do
     echo "Pulling image localhost:5000/$image from local registry into daemon..."
     docker pull localhost:5000/"$image"
-    echo "Re-tagging image as veracioux.me/$image"
-    docker tag localhost:5000/"$image" veracioux.me/"$image"
+    echo "Re-tagging image as docker.veracioux.me/$image"
+    docker tag localhost:5000/"$image" docker.veracioux.me/"$image"
 done
 
