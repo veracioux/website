11fdfda82af547499a9abcd56a14c3e4752c6ba1
Haris Gušić <harisgusic.dev@gmail.com>
2022-12-03 18:33:33 +0100

    Proxy 'frontend_dev' service through 'web'

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,7 +5,7 @@ echo "Running web server on port $WEB_PORT"
 set -e
 
 jinja2 -D env="$ENVIRONMENT" -D device="$DEVICE" nginx.conf.in \
-    | envsubst '$WEB_PORT,$WEB_PORT_STAGING,$BACKEND_HOST,$BACKEND_PORT,$WORKER_SERVER_HOST,$WORKER_SERVER_PORT' \
+    | envsubst '$WEB_PORT,$WEB_PORT_STAGING,$FRONTEND_DEV_HOST,$FRONTEND_DEV_PORT,$BACKEND_HOST,$BACKEND_PORT,$WORKER_SERVER_HOST,$WORKER_SERVER_PORT' \
     > /etc/nginx/nginx.conf
 
 php-fpm81
