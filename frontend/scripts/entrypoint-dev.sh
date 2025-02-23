27e864fafed62e62fe4d8ddf18284f7eb6a7751c
Haris Gušić <harisgusic.dev@gmail.com>
2025-02-23 18:34:35 +0100

    refactor: Simply dockerfiles and compose files using modern features

--- frontend/scripts/entrypoint-dev.sh
+++ frontend/scripts/entrypoint-dev.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-# TODO: move this comment to narrative documentation
-# Because the project root directory on the host is being shared with the
-# container as a volume, we have to re-populate the node_modules directories at
-# runtime. This is a cheap operation since the modules have already been
-# installed during docker build, so pnpm wll simply create hardlinks to them.
-rm -rf /app/node_modules
-pnpm install --shamefully-hoist --frozen-lockfile --no-verify-store-integrity
+pnpm install --shamefully-hoist --frozen-lockfile
 
-pnpm run dev-container
+pnpm run dev:container
