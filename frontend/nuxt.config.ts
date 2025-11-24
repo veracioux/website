327d8b5dd762864cc8a05ce9f9085b0e769f56e2
Haris Gušić <harisgusic.dev@gmail.com>
2025-11-24 19:59:35 +0100

    chore: Set up tailwind

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,7 +6,7 @@ import { defineNuxtConfig, type NuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   compatibilityDate: "2025-11-23",
   // buildModules: ["@nuxtjs/google-fonts"],
-  modules: ["@nuxtjs/google-fonts"],
+  modules: ["@nuxtjs/google-fonts", "@nuxtjs/tailwindcss"],
   srcDir: "src/",
   runtimeConfig: {
     public: {
