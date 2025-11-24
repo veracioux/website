15d76749423ce2370e35b6214aff9859f10fd261
Haris Gušić <harisgusic.dev@gmail.com>
2025-11-24 19:44:09 +0100

    feat: Add /projects page

---
+++ frontend/src/pages/projects.vue
@@ -0,0 +1,11 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="tsx">
+import PageWithNavbar from "@/components/PageWithNavbar.vue";
+import Projects from "@/components/sections/Projects.vue";
+</script>
+
+<template>
+  <PageWithNavbar>
+    <Projects />
+  </PageWithNavbar>
+</template>
