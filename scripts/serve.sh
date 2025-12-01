f79ce02ff129fa14b21a8885a468163494f38251
Haris Gušić <harisgusic.dev@gmail.com>
2025-12-01 19:22:13 +0100

    fix: Fix scripts

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -16,6 +16,21 @@ if nginx -t 2>/dev/null; then
     cp /etc/nginx/nginx.conf{,.bak}
 fi
 
+if [ -z "$WEB_PORT" ]; then
+    echo "Error: Please set WEB_PORT environment variable in the .env.d files." >&2
+    exit 1
+fi
+
+if [ -z "$WEB_PORT_STAGING" ]; then
+    echo "Error: Please set WEB_PORT_STAGING environment variable in the .env.d files." >&2
+    exit 1
+fi
+
+if [ -z "$DOCKER_REGISTRY_PORT" ]; then
+    echo "Error: Please set DOCKER_REGISTRY_PORT environment variable in the .env.d files." >&2
+    exit 1
+fi
+
 cat host/nginx.conf.in | envsubst '$WEB_PORT,$WEB_PORT_STAGING,$USER_HOME,$DOCKER_REGISTRY_PORT' > /etc/nginx/nginx.conf
 
 config_check_output="$(nginx -t 2>&1)"
