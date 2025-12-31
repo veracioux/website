caa9522c4ef392f9a89c0c2dce391b021e2358ce
Haris Gušić <harisgusic.dev@gmail.com>
2025-12-31 23:21:20 +0100

    feat: Add dev mode detection for CLI

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -5,13 +5,37 @@ import { hideBin } from "yargs/helpers";
 import cvCommand from "./cv";
 import passCommand from "./pass";
 import webCommand from "./web";
+import { failExit, findUpwardFile, getEnv } from "./lib";
+import path from "path";
+import chalk from "chalk";
 
-const cli = yargs(hideBin(process.argv))
-  .scriptName("@veracioux/cli")
-  .command(cvCommand)
-  .command(passCommand)
-  .command(webCommand)
-  .help()
-  .alias("help", "h");
+const env = await getEnv().catch((e) => failExit(e.message));
 
-await cli.parseAsync();
+if (env === "dev" && !process.env.VERACIOUX_IS_DEV_CLI) {
+  const projectRoot = path.dirname(
+    (await findUpwardFile(process.cwd(), ".veracioux-website-repo-root"))!
+  );
+  console.log(chalk.yellow.bold("Running local @veracioux/cli in dev mode..."));
+  await Bun.spawn({
+    cmd: ["bun", "run", "cli", ...hideBin(process.argv)],
+    cwd: projectRoot,
+    stdout: "inherit",
+    env: {
+      ...process.env,
+      BUN_OPTIONS: "",
+      NODE_OPTIONS: "",
+    }
+  }).exited;
+} else {
+  const cli = yargs(hideBin(process.argv))
+    .scriptName(
+      process.argv[0] === "veracioux" ? "veracioux" : "@veracioux/cli"
+    )
+    .command(cvCommand)
+    .command(passCommand)
+    .command(webCommand)
+    .help()
+    .alias("help", "h");
+
+  await cli.parseAsync();
+}
