920a62fb31dff30ae228e500ed1a7da56c020eb6
Haris Gušić <harisgusic.dev@gmail.com>
2025-12-31 19:44:40 +0100

    fix: Add undici and configure rejectUnauthorized: false

--- cli/src/web.ts
+++ cli/src/web.ts
@@ -3,7 +3,8 @@ import { exec } from "child_process";
 import { promisify } from "util";
 import chalk from "chalk";
 import { cmd } from "./lib";
-import { iife, withDispose } from "@veracioux/lib";
+import { iife } from "@veracioux/lib";
+import undici from "undici";
 
 const execAsync = promisify(exec);
 
@@ -15,9 +16,7 @@ export default cmd({
       command: "info",
       describe: "Show web server info",
       async handler() {
-        const previousReject = process.env.NODE_TLS_REJECT_UNAUTHORIZED;
-        await using _ = withDispose(() => process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0", () => { process.env.NODE_TLS_REJECT_UNAUTHORIZED = previousReject; });
-        const domains = ["veracioux.me", "stg.veracioux.me", "veracioux.local"];
+        const domains = ["veracioux.me", "stg.veracioux.me"];
         const promises = [];
         const timeout = 1000;
         for (const domain of domains) {
@@ -27,25 +26,32 @@ export default cmd({
                 const addresses = await dns.promises.resolve4(domain);
                 let status: string = "";
                 try {
-                  await execAsync(`ping -c 1 ${domain}`, { signal: AbortSignal.timeout(timeout) });
-                  status += "  " + chalk.green(`PING ✅`);
-                } catch (e: any) {
-                  if (e.name === "AbortError") 
-                    status += "  " + chalk.red(`PING ⌛`);
-                  else
-                    status += "  " + chalk.red(`PING ❌`);
-                }
-                try {
-                  const response = await fetch(`https://${domain}/status`, {
+                  await execAsync(`ping -c 1 ${domain}`, {
                     signal: AbortSignal.timeout(timeout),
                   });
+                  status += "  " + chalk.green(`PING ✅`);
+                } catch (e: any) {
+                  if (e.name === "AbortError")
+                    status += "  " + chalk.red(`PING ⌛`);
+                  else status += "  " + chalk.red(`PING ❌`);
+                }
+                try {
+                  const response = await undici.fetch(
+                    `https://${domain}/status`,
+                    {
+                      signal: AbortSignal.timeout(timeout),
+                      dispatcher: new undici.Agent({
+                        connect: { rejectUnauthorized: false },
+                      }),
+                    }
+                  );
                   const statusEmoji = response.ok ? "✅" : "❌";
                   status += "  " + chalk.green(`/status ${statusEmoji}`);
                 } catch (e: any) {
-                    if (e.name === "AbortError")
-                      status += "  " + chalk.red(`/status ⌛`);
-                    else
-                      status += "  " + chalk.red(`/status ❌`);
+                  console.error(e);
+                  if (e.name === "AbortError")
+                    status += "  " + chalk.red(`/status ⌛`);
+                  else status += "  " + chalk.red(`/status ❌`);
                 }
                 console.log(`${chalk.bold(domain)}: ${addresses.join(", ")}`);
                 console.log(status);
