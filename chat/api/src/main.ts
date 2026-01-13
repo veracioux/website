f7137d407a56de4f3f1450a4613acb9e1aabcf3e
Haris Gušić <harisgusic.dev@gmail.com>
2026-01-13 19:58:02 +0100

    feat: Add Swagger endpoint at /swagger
    Using Nestia Swagger Composer to generate and serve OpenAPI documentation at /swagger.

--- chat/api/src/main.ts
+++ chat/api/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { NestiaSwaggerComposer } from "@nestia/sdk";
+import { SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const document = await NestiaSwaggerComposer.document(app, {});
+  SwaggerModule.setup("swagger", app, document as any);
   await app.listen(process.env.PORT ?? 9000);
 }
 bootstrap();
